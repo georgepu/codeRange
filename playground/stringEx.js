b58055e634a51686fead637772d66bd8e748dc4b
Qiaoyuan Pu <georgepu888@gmail.com>
2019-07-05 00:11:08 -0400

    string#6-8

--- playground/stringEx.js
+++ playground/stringEx.js
@@ -36,12 +36,6 @@ function truncate_string(str, n) {
 	return str.trim().substr(0, n);
 }
 
-// 9. Write a JavaScript function to capitalize the first letter of each word in a string.
-// Test Data :
-console.log(capitalize_Words('js string exercises'));
-// "Js String Exercises"
-function capitalize_Words(str) {
-	return str.split(' ').map(capitalize);
 // 5. Write a JavaScript function to convert a string in abbreviated form.
 // Test Data :
 console.log(abbrev_name('Robin Singh'));
@@ -50,4 +44,46 @@ function abbrev_name(str) {
 	const split = str.split(' ');
 	const letter = split[split.length - 1].slice(0, 1);
 	return `${split[0]} ${letter}.`;
+}
+
+// 6. Write a JavaScript function to hide email addresses to protect from unauthorized user.
+// Test Data :
+console.log(protect_email('robin_singh@example.com'));
+// "robin...@example.com"
+function protect_email(str) {
+	const split = str.split('@');
+	const usr = `${split[0].split('_')[0]}...`;
+	return Array(usr)
+		.concat(split[1])
+		.join('@');
+}
+
+// 7. Write a JavaScript function to parameterize a string.
+// Test Data :
+console.log(string_parameterize('Robin Singh from USA.'));
+// "robin-singh-from-usa"
+function string_parameterize(str) {
+	return str
+		.toLowerCase()
+		.replace(/.$/, '')
+		.split(' ')
+		.join('-');
+}
+
+// 8. Write a JavaScript function to capitalize the first letter of a string.
+// Test Data :
+console.log(capitalize('js string exercises'));
+// "Js string exercises"
+function capitalize(str) {
+	return str.match(/^[a-z]/g)
+		? str.replace(str.charAt(0), str.charAt(0).toUpperCase())
+		: str;
+}
+
+// 9. Write a JavaScript function to capitalize the first letter of each word in a string.
+// Test Data :
+console.log(capitalize_Words('js string exercises'));
+// "Js String Exercises"
+function capitalize_Words(str) {
+	return str.split(' ').map(capitalize);
 }
--- practice.js
+++ practice.js
@@ -7,29 +7,29 @@ const ar2 = Array('1', '2', '3', '4');
 const ar3 = ['5', '6', '7', '8'];
 console.log(ar1);
 (function display(ary) {
-  for (member of ary) {
-    console.log(member);
-  }
+	for (member of ary) {
+		console.log(member);
+	}
 }(ar1));
 
 // associative array for (key in emp) {}
 const obj1 = {
-  a: 1,
-  b: 2,
-  c: 3,
-  d: 4,
+	a: 1,
+	b: 2,
+	c: 3,
+	d: 4,
 };
 (function showObj(obj) {
-  for (key in obj) {
-    console.log(obj[key]);
-  }
+	for (key in obj) {
+		console.log(obj[key]);
+	}
 }(obj1));
 
 const test = [];
 test.hi = 2;
 test.gee = 4;
 test.fun = () => {
-  console.log('look at this');
+	console.log('look at this');
 };
 test[4] = 10;
 test.push('hello');
@@ -50,12 +50,12 @@ console.log(`${myObj.a}     ${myObj.b}
   ${myObj.c} hello ${myObj.d}`);
 
 function MyConstructor(a, b) {
-  this.name = a;
-  this.age = b;
-  MyConstructor.prototype.doIt = () => {
-    console.log("I'm getting there,my friend!");
-    return this;
-  };
+	this.name = a;
+	this.age = b;
+	MyConstructor.prototype.doIt = () => {
+		console.log('I\'m getting there,my friend!');
+		return this;
+	};
 }
 
 const obj = new MyConstructor('George', 24);
@@ -66,24 +66,24 @@ console.log(toString(myObj));
 
 // deal with circular obj
 const customStringify = (v) => {
-  const cache = new Set();
-  return JSON.stringify(v, (key, value) => {
-    if (typeof value === 'object' && value !== null) {
-      if (cache.has(value)) {
-        // Circular reference found
-        try {
-          // If this value does not reference a parent it can be deduped
-          return JSON.parse(JSON.stringify(value));
-        } catch (err) {
-          // discard key if value cannot be deduped
-          return;
-        }
-      }
-      // Store value in our set
-      cache.add(value);
-    }
-    return value;
-  });
+	const cache = new Set();
+	return JSON.stringify(v, (key, value) => {
+		if (typeof value === 'object' && value !== null) {
+			if (cache.has(value)) {
+				// Circular reference found
+				try {
+					// If this value does not reference a parent it can be deduped
+					return JSON.parse(JSON.stringify(value));
+				} catch (err) {
+					// discard key if value cannot be deduped
+					return;
+				}
+			}
+			// Store value in our set
+			cache.add(value);
+		}
+		return value;
+	});
 };
 // import { inspect } from 'util';
 const util = require('util');
@@ -100,69 +100,69 @@ console.log(util.inspect(a));
 
 // interview question
 const oo = {
-  name: 'George',
-  myFunc() {
-    console.log(`hey dude!${this.name}`);
-  },
+	name: 'George',
+	myFunc() {
+		console.log(`hey dude!${this.name}`);
+	},
 };
 oo.myFunc();
 
 function pp(cb) {
-  cb();
+	cb();
 }
 
 pp(oo.myFunc);
 
 // interview question
 for (let i = 0; i <= 5; i++) {
-  (function (j) {
-    setTimeout(() => {
-      console.log(j);
-    }, 0);
-  }(i));
+	(function (j) {
+		setTimeout(() => {
+			console.log(j);
+		}, 0);
+	}(i));
 }
 
 // interview question
 function bad(person) {
-  if (
-    JSON.stringify(person)
+	if (
+		JSON.stringify(person)
     === JSON.stringify({
-      name: 'Lian',
+    	name: 'Lian',
     })
-  ) {
-    console.log('hey');
-  } else {
-    console.log('ha');
-  }
+	) {
+		console.log('hey');
+	} else {
+		console.log('ha');
+	}
 }
 bad({
-  name: 'Lian',
+	name: 'Lian',
 });
 
 // ES6 class constructor
 class Vehicle {
-  constructor(make, model, year) {
-    this.make = make;
-    this.model = model;
-    this.year = year;
-  }
+	constructor(make, model, year) {
+		this.make = make;
+		this.model = model;
+		this.year = year;
+	}
 
-  print() {
-    console.log(`This vehicle is: ${this.make} ${this.model} ${this.year}.`);
-  }
+	print() {
+		console.log(`This vehicle is: ${this.make} ${this.model} ${this.year}.`);
+	}
 
-  disclaimer() {
-    console.log('This is an exercise.');
-  }
+	disclaimer() {
+		console.log('This is an exercise.');
+	}
 }
 
 class Suv extends Vehicle {
-  /* constructor (make, model, year, size) {
+	/* constructor (make, model, year, size) {
     super(make, model, year)
   } */
-  disclaimer() {
-    console.log('This is a practice.');
-  }
+	disclaimer() {
+		console.log('This is a practice.');
+	}
 }
 const vehicle = new Vehicle('Jeep', 'Grand Cherokee', '2017');
 vehicle.disclaimer();
@@ -172,52 +172,52 @@ jeep.disclaimer();
 
 // Get person with max salary
 const salaries = {
-  John: 100,
-  Pete: 300,
-  Mary: 250,
+	John: 100,
+	Pete: 300,
+	Mary: 250,
 };
 const dummy = {};
 ((a) => {
-  const [name = null] = Object.entries(a).sort((x, y) => y[1] - x[1])[0] || [];
-  console.log(name);
+	const [name = null] = Object.entries(a).sort((x, y) => y[1] - x[1])[0] || [];
+	console.log(name);
 })(dummy);
 
 const boss = {
-  ele: 888,
-  bobby() {
-    const ele = 88888;
-    const amen = function () {
-      console.log(this.ele);
-    }.bind(this);
-    amen();
-  },
-  anotherObj: {
-    ele: 6969,
-    lynn() {
-      const george = () => {
-        const ele = 9090;
-        console.log(this.ele);
-      };
-      george();
-    },
-  },
+	ele: 888,
+	bobby() {
+		const ele = 88888;
+		const amen = function () {
+			console.log(this.ele);
+		}.bind(this);
+		amen();
+	},
+	anotherObj: {
+		ele: 6969,
+		lynn() {
+			const george = () => {
+				const ele = 9090;
+				console.log(this.ele);
+			};
+			george();
+		},
+	},
 };
 global.ele = 8970;
 boss.bobby();
 boss.anotherObj.lynn();
 
 function foo(x = 42) {
-  console.log(x);
+	console.log(x);
 }
 foo([,,]);
 
 global.x = 8;
 {
-  global.x = 10;
-  function weird(x = 2, f = () => x) {
-    { const x = 5;
-      console.log(f());
-      console.log(x); }
-  }
-  weird(3, () => x * 4);
+	global.x = 10;
+	function weird(x = 2, f = () => x) {
+		 var x = 5;
+		console.log(f());
+		console.log(x); 
+	}
+	weird();
 }
